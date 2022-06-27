95169cf6ec6422720b78ffac9bc6df9e93b6581c
Juan Ramirez <rami8224.2007@gmail.com>
2022-06-27 15:41:18 -0400

    intit commit

---
+++ src/pages/blogs.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import { Routes, Route, Link } from "react-router-dom";
+import Navbar from './components/navbar';
+
+const Blogs = () => {
+  return (
+    <div>
+        <Navbar/>
+        <h1>Blogs Page</h1>
+    </div>
+  )
+}
+
+
+export default Blogs;
